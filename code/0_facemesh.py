16439d6556d14cc1358ff8edf8986aa0e198d30a
ko-watanabe <ko.watanabe.0522@gmail.com>
2021-11-09 13:48:25 +0100

    remove file and fix code

--- code/0_facemesh.py
+++ code/0_facemesh.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture("./dataset/result_multi.mp4")
+cap = cv2.VideoCapture("dataset/result_multi.mp4")
 pTime = 0
 
 mpDraw = mp.solutions.drawing_utils
